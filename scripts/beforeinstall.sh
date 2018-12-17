3657dfdf81c5da6325a9bb0dd797af7d11275823
root <root@ip-172-31-27-49.ap-south-1.compute.internal>
2018-12-17 17:36:41 +0000

    done

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -5,31 +5,36 @@ sudo apt-get install curl python-software-properties
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 # install node.js
-apt install nodejs -y
+sudo apt install nodejs -y
 #sudo npm install -g pm2
 sudo apt-get install npm -y
 
 # create systemd service
-cat > /etc/systemd/system/nodejs.service <<'EOF'
-[Unit]
-Description=Node.js App
-After=network.target
-[Service]
-User=nobody
-WorkingDirectory=/var/www/nodejs
-ExecStart=/usr/bin/npm run start:dev
-Restart=always
-RestartSec=500ms
-StartLimitInterval=0
-[Install]
-WantedBy=multi-user.target
+:
+# create springboot service
+cat > /etc/init/nodejs.conf <<'EOF'
+description Node.js App
+  start on runlevel [2345]
+  stop on runlevel [!2345]
+  respawn
+  respawn limit 10 5
+  # run as non privileged user 
+  # add user with this command:
+  ## adduser --system --ingroup www-data --home /opt/apache-tomcat apache-tomcat
+  # Ubuntu 12.04: (use 'exec sudo -u apache-tomcat' when using 10.04)
+  setuid ubuntu
+  setgid ubuntu
+  # adapt paths:
+  
+  exec /usr/bin/npm run start:dev
+  # cleanup temp directory after stop
+  post-stop script
+  end script
 EOF
 
-
-
 # reload systemd services
-systemctl daemon-reload
-systemctl enable nodejs.service
+sudo initctl reload-configuration
+sudo initctl enable nodejs.service
 
 # remove old directory
 rm -rf /var/www/nodejs
